776184a516f2658384ce4fd873b279dcc1c621c0
Tim Stalker <Tim.Stalker@ucdenver.edu>
2019-08-20 11:48:00 -0600

    removing brand bar color form fixing layers footer graphic

--- themes/ucb/templates/region--branding.tpl.php
+++ themes/ucb/templates/region--branding.tpl.php
@@ -1,8 +1,7 @@
 <?php
   global $base_url;
-  // Get logo/brand bar color.
-	$color = !empty(theme_get_setting('brand_bar_color')) ? theme_get_setting('brand_bar_color') : 'black';
-	$logo = $base_url . '/' . drupal_get_path('theme', 'ucb') . '/images/cu-boulder-logo-text-' . $color . '.svg';
+  // Set brand bar color.
+	$color = isset($logo_color) ? $logo_color : 'black';
 
 ?>
 <div class="brand-bar brand-bar-color-<?php print $color; ?> no-underline">
@@ -15,7 +14,7 @@
       </div>
       <div class="search-toggle-wrapper">
         <a href="http://myemail.ucdenver.edu/" class="search-toggle-link">Webmail <span style="padding:0px 5px 0px 5px">|</span></a> <a href="https://portal.prod.cu.edu/UCDAccessFedAuthLogin.html" class="search-toggle-link">UCD Access <span style="padding:0px 5px 0px 5px">|</span></a> <a href="http://www.ucdenver.edu/courselogin" class="search-toggle-link">Canvas <span style="padding:0px 5px 0px 5px">|</span></a> <a href="#quickLinks" class="search-toggle-quicklinks">Quick Links</a>
-        <a href="https://www.colorado.edu/search" id="search-toggle" class="search-toggle" aria-haspopup="true" aria-expanded="false" aria-controls="search" aria-label="Search"><i class="fa fa-search fa-fw"></i><span class="element-invisible">Search </span></a>
+        <a href="https://clas.ucdenver.edu/university-search/" id="search-toggle" class="search-toggle" aria-haspopup="true" aria-expanded="false" aria-controls="search" aria-label="Search"><i class="fa fa-search fa-fw"></i><span class="element-invisible">Search </span></a>
       </div>
     </div>
   </div>
