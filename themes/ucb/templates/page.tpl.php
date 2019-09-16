ff3f5d806b88ac27f967eb968d1d88a22a408ba9
Tim Stalker <Tim.Stalker@ucdenver.edu>
2019-09-16 14:33:54 -0600

    fixing scrolling quicklinks in mobile

--- themes/ucb/templates/page.tpl.php
+++ themes/ucb/templates/page.tpl.php
@@ -30,6 +30,67 @@
     <!-- SITE HEADER -->
     <div id="header-wrapper" class="section header-wrapper no-underline cu-header">
       <div class="container">
+        <div class="u-mask">
+          <div class="t-contentBlock u-mask">
+
+           <div id="quickLinks" class="t-quickLinks" aria-label="Quick links navigation" aria-hidden="true" aria-expanded="false">
+             <div class="container">
+               <div class="col-md-3" style="float:left">
+               <h3>Locations</h3>
+               <ul class="list-unstyled">
+                 <li><a href="http://www.ucdenver.edu/">CU Denver</a></li>
+                 <li><a href="https://www.cuanschutz.edu">CU Anschutz Medical Campus</a></li>
+                 <li><a href="https://southdenver.cu.edu">CU South Denver</a></li>
+                 <li><a href="https://www1.ucdenver.edu/online">CU Online</a></li>
+               </ul>
+               </div>
+               <div class="col-md-3" style="float:left">
+               <h3>Tools</h3>
+               <ul class="list-unstyled">
+                 <li><a href="http://myemail.ucdenver.edu/">Webmail</a></li>
+                 <li><a href="https://portal.prod.cu.edu/UCDAccessFedAuthLogin.html">UCDAccess</a></li>
+                 <li><a href="http://www.ucdenver.edu/courselogin">Canvas</a></li>
+                 <li><a href="https://directory.ucdenver.edu/">Directory</a></li>
+                 <li><a href="http://www.ucdenver.edu/AZIndex/Pages/A-ZIndex.aspx">A-Z</a></li>
+                 <li><a href="http://www.ucdenver.edu/faculty-staff/">Faculty &amp; Staff</a></li>
+                 <li><a href="http://library.auraria.edu/">Auraria Library</a></li>
+                 <li><a href="http://hslibrary.ucdenver.edu/">Health Sciences Library</a></li>
+               </ul>
+               </div>
+               <div class="col-md-6">
+               <h3>Schools and Colleges</h3>
+               <div class="row">
+                 <div class="col-md-6" style="float:left">
+                   <h4>CU Denver</h4>
+                   <ul class="list-unstyled">
+                     <li><a href="https://architectureandplanning.ucdenver.edu/">College of Architecture and Planning</a></li>
+                     <li><a href="https://artsandmedia.ucdenver.edu">College of Arts and Media</a></li>
+                     <li><a href="https://business.ucdenver.edu">Business School</a></li>
+                     <li><a href="https://education.ucdenver.edu">School of Education and Human Development</a></li>
+                     <li><a href="https://engineering.ucdenver.edu">College of Engineering and Applied Science</a></li>
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/Graduate-School/">Graduate School</a></li>
+                     <li><a href="https://clas.ucdenver.edu">College of Liberal Arts and Sciences</a></li>
+                     <li><a href="https://publicaffairs.ucdenver.edu">School of Public Affairs</a></li>
+                   </ul>
+                 </div>
+                 <div class="col-md-6">
+                   <h4>CU Anschutz Medical Campus</h4>
+                   <ul class="list-unstyled">
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/dentalmedicine/">School of Dental Medicine</a></li>
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/Graduate-School/">Graduate School</a></li>
+                     <li><a href="https://medschool.cuanschutz.edu">School of Medicine</a></li>
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/nursing/">College of Nursing</a></li>
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/pharmacy/">Skaggs School of Pharmacy and Pharmaceutical Sciences</a></li>
+                     <li><a href="http://www.ucdenver.edu/academics/colleges/PublicHealth/">Colorado School of Public Health</a></li>
+                   </ul>
+                 </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+           <div class="clearfix"></div>
+          </div>
+        </div>
       <header class="ucb header clearfix" id="header" role="banner" tabindex="-1">
         <div class="site-name-wrapper">
 
