5571a6ad2dbfabd70469662dee946d428c530a9c
Tim Stalker <Tim.Stalker@ucdenver.edu>
2019-11-18 12:41:43 -0700

    added salesforce js for business

--- themes/ucb/template.php
+++ themes/ucb/template.php
@@ -190,8 +190,9 @@ function ucb_preprocess_html(&$vars) {
   $vars['classes_array'][] = 'express-site-type-' . variable_get('express_site_type', 'default');
   $vars['classes_array'][] = 'brand-fonts';
   
-  //Add cactus communications google tag container for OIT
+  //Add cactus communications google tag container for OIT and salesforce js for business
   if (isset($_SERVER['WWWNG_BIZ'])) {
+  	drupal_add_js(drupal_get_path('theme','ucb') .'/js/scriptResource4.js', array('scope' => 'header'));
     $gtmbizcontain = <<<EOS
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5DDZ4Z3')
 EOS;
