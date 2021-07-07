a9b61c0bddbd1098c6af7271825fa532f481775e
Tim Stalker <Tim.Stalker@ucdenver.edu>
2021-07-07 12:10:00 -0600

    adding gtm codes back in

--- themes/ucb/template.php
+++ themes/ucb/template.php
@@ -193,6 +193,16 @@ function ucb_preprocess_html(&$vars) {
 
   // This class is added to use the Helvetica Neue brand fonts.
   $vars['classes_array'][] = 'brand-fonts';
+  
+  if (isset($_SERVER['WWWNG_ENV'])) {
+    $gtmclascontain = <<<EOS
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KCBXJSR')
+EOS;
+    drupal_add_js($gtmclascontain, array('type' => 'inline', 'scope' => 'header', 'weight' => 5));
+  }
+  else {
+    return;
+  }
 }
 
 /**
